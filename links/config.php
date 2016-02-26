0b24ad04ddfbb6fabbccf41d6d5885f2eb1fc4df
gummybites <kennethberdensen@gmail.com>
2016-02-25 18:59:15 -0800

    Feb 26, 2016
    Full system update

--- links/config.php
+++ links/config.php
@@ -1,16 +1,16 @@
 <?php 
 
 
-//		$dbLocalhost = mysql_connect("mysql.hostinger.ph", "u612571292_mks", "iOwJvyeS42")
-//	or die("Could not connect: " . mysql_error());
-//	mysql_select_db("u612571292_mks", $dbLocalhost)
-//		or die("Could not find database: " . mysql_error());
-
-
-  $dbLocalhost = mysql_connect("localhost", "root", "")
+		$dbLocalhost = mysql_connect("mysql.hostinger.ph", "u612571292_mks", "iOwJvyeS42")
 	or die("Could not connect: " . mysql_error());
-	mysql_select_db("mks", $dbLocalhost)
-	or die("Could not find database: " . mysql_error());
+	mysql_select_db("u612571292_mks", $dbLocalhost)
+		or die("Could not find database: " . mysql_error());
+
+
+//  $dbLocalhost = mysql_connect("localhost", "root", "")
+//	or die("Could not connect: " . mysql_error());
+//	mysql_select_db("mks", $dbLocalhost)
+//	or die("Could not find database: " . mysql_error());
 ?>
 
 
